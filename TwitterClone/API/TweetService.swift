49005de6a9ecfb75009fcf6e59c584f9f7fbe972
yolomiyaba <ac3po6@gmail.com>
2023-07-11 08:58:07 +0900

    Added model for tweet and functionality to fetch tweets

--- TwitterClone/API/TweetService.swift
+++ TwitterClone/API/TweetService.swift
@@ -17,6 +17,17 @@ struct TweetService {
         //creates child with automatic uuid (for each tweet)
         REF_TWEETS.childByAutoId().updateChildValues(values, withCompletionBlock: completion)
     }
+    
+    func fetchTweets(completion: @escaping([Tweet]) -> Void){
+        var tweets = [Tweet]()
+        
+        //childAdded observes the database all the time, making it possible for tweets too show up as soon as they have been posted
+        REF_TWEETS.observe(.childAdded) { snapshot in
+            guard let dictionary = snapshot.value as? [String: Any] else {return}
+            let tweetID = snapshot.key
+            let tweet = Tweet(tweetID: tweetID, dictionary: dictionary)
+            tweets.append(tweet)
+            completion(tweets)
+        }
+    }
 }
-
-
